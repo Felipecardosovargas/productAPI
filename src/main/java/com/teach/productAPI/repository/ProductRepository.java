66c1612d6ef066e5caca2c4d30ca0acc8c5c71b4
Felipe Cardoso Vargas <128812730+Felipecardosovargas@users.noreply.github.com>
2025-06-30 19:40:41 -0300

    feat(repository): create ProductRepository interface with basic methods

--- src/main/java/com/teach/productAPI/repository/ProductRepository.java
+++ src/main/java/com/teach/productAPI/repository/ProductRepository.java
@@ -1,4 +1,14 @@
 package com.teach.productAPI.repository;
 
-public class ProductRepository {
+import com.teach.productAPI.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    //            findBy[createDate]Between
+    List<Product> findByCreateDateBetween(LocalDateTime startDate, LocalDateTime endDate);
 }
